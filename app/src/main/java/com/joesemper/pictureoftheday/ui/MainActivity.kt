3e473a53eff708d00f8e074259953a2709559746
JoeSemper <joe.semper@gmail.com>
2021-03-22 09:31:30 +0300

    hw_1 base commit

--- app/src/main/java/com/joesemper/pictureoftheday/ui/MainActivity.kt
+++ app/src/main/java/com/joesemper/pictureoftheday/ui/MainActivity.kt
@@ -1,4 +1,19 @@
 package com.joesemper.pictureoftheday.ui
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.joesemper.pictureoftheday.R
+import com.joesemper.pictureoftheday.ui.picture.PictureOfTheDayFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PictureOfTheDayFragment.newInstance())
+                .commitNow()
+        }
+    }
 }
